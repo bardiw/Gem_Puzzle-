0ee2259f8220e2d50489d126f5fe74e4ffa02953
bardiw <96146827+bardiw@users.noreply.github.com>
2023-01-08 15:58:23 +0330

    sign up & sign in & username
    sign up & sign in & username

--- main.cpp
+++ main.cpp
@@ -1,60 +1,97 @@
 #include <iostream>
-#include <numeric>
+#include <vector>
+#include <cstdlib>
 #include <ctime>
-using namespace std ;
-int ** make_arr(int n)
-{
-    srand(time(0)) ;
-    int ** arr = new int * [n] ;
-    for (int i = 0 ; i < n ; i++){
-        arr[i] = new int [n] ;
-    }
-    int c = 1 ;
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < n ; j++){
-            arr[i][j] = c ;
-            c++ ;
+#include<windows.h>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <unordered_map>
+using namespace std;
+const string ACCOUNTS_FILE = "accounts.txt";
+struct Account {
+    string username;
+    string password;
+    int score;
+};
+unordered_map<string, Account> accounts;
+
+string signUp() {
+    string username, password;
+    cout << "Enter a new username: ";
+    cin >> username;
+
+    bool passwordValid = false;
+    while (!passwordValid) {
+        cout << "Enter a new password: ";
+        cin >> password;
+
+        regex passwordRegex("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*]).{8,}$");
+        if (!regex_match(password, passwordRegex)) {
+            cout << "Error: password is not strong enough. It must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character." << endl;
+        } else {
+            passwordValid = true;
         }
     }
-    arr[n-1][n-1] = 0 ;
-    int arr2[n] ;
-    for (int i = 0 ; i < n ; i++){
-        arr2[i] = -1 ;
-    }
-    int a = 0 ;
-    for(int k = 0 ; k < n;k++){
-g :
-        int r2 = rand() % n ;
-        for (int i = 0 ; i < n ; i++){
-            if (arr2[i] != r2 ){
-                a++ ;
-            }
-        }
-        if (a == n){
-            arr2[k] = r2 ;
-            a = 0 ;
-        }
-        else{
-            a = 0 ;
-            goto g ;
-        }
-        for(int m = 0 ; m < n ; m++){
-            arr[r2][m] = arr[m][m] ; //For debugging
-        }
-        cout << endl;
-    }
-    return arr ;
+
+    Account account = {username, password, 0};
+    accounts[username] = account;
+
+    ofstream accountsFile(ACCOUNTS_FILE, ios::app);
+    accountsFile << username << " " << password << " " << 0 << endl;
+    accountsFile.close();
+    return username ;
 }
-int main ()
-{
-    int num ;
-    cin >> num ;
-    int ** arr = make_arr(num) ;
-    for (int i = 0 ; i < num ; i++){
-        for (int j = 0 ; j < num ; j++){
-            cout << arr[i][j] << " " ;
-        }
-        cout << endl ;
+
+string signIn() {
+    string username, password;
+    cout << "Enter your username: ";
+    cin >> username;
+    cout << "Enter your password: ";
+    cin >> password;
+
+    if (accounts.count(username) && accounts[username].password == password) {
+        cout << "Welcome back, " << username << "!" << endl;
+        return username ;
+    } else {
+        cout << "Invalid username or password." << endl;
+        return "" ;
     }
-    return 0 ;
 }
+
+string username_acount(string user= ""){
+    while(user=="")
+    {
+        cout << "Would you like to sign up (1) or sign in (2)? ";
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            user = signUp();
+        } else if (choice == 2) {
+            user = signIn();
+        } else {
+            cout << "Invalid choice." << endl;
+        }
+    }
+    return user;
+}
+
+int main()
+{
+    string user;
+    srand(time(0));
+
+    ifstream accountsFile(ACCOUNTS_FILE);
+    string username, password;
+    int score;
+    while (accountsFile >> username >> password >> score) {
+        Account account = {username, password, score};
+        accounts[username] = account;
+    }
+    accountsFile.close();
+
+    user = username_acount() ;
+        return 0;
+}
+
